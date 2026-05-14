42020d684aa5133f205fda970fd158c223d96838
MADIGITAL\virgilio.jose <virgilio.jose@inovadigital.eu>
2026-05-14 14:17:09 +0100

    feat: update Firebase configuration to fetch from Netlify functions for improved security

--- netlify/js/firebase.js
+++ netlify/js/firebase.js
@@ -3,14 +3,10 @@ import { initializeApp } from "https://www.gstatic.com/firebasejs/10.7.1/firebas
 import { getFirestore } from "https://www.gstatic.com/firebasejs/10.7.1/firebase-firestore.js";
 import { getAuth } from "https://www.gstatic.com/firebasejs/10.7.1/firebase-auth.js";
 
-const firebaseConfig = {
-  apiKey: "AIzaSyD2DiY5Gqm2pdflZYb2Lx75oLheCo-qPAQ",
-  authDomain: "clothesshop-43d13.firebaseapp.com",
-  projectId: "clothesshop-43d13",
-  storageBucket: "clothesshop-43d13.firebasestorage.app",
-  messagingSenderId: "213129064111",
-  appId: "1:213129064111:web:0471e3c390ca00dbc02e5a"
-};
+const res = await fetch('/.netlify/functions/firebase-config');
+if (!res.ok) throw new Error('Não foi possível carregar a configuração. Usa "netlify dev" para desenvolvimento local.');
+const firebaseConfig = await res.json();
+if (firebaseConfig.error) throw new Error(firebaseConfig.error);
 
 const app = initializeApp(firebaseConfig);
 export const db = getFirestore(app);
